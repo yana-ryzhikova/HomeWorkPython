6ebae3537ec77f7a26bf8814a578663da925e2dc
Yana Ryzhikova <ryzhikovayana@gmail.com>
2022-12-19 14:42:31 +0300

    changed seminar02

--- Seminar02/task03.py
+++ Seminar02/task03.py
@@ -4,17 +4,6 @@
 
 # - Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
 
-# n = int(input('Введите число N -> '))
-# my_list = []
-# for i in range(1, n + 1):
-#     my_list[i] = round((1 + 1 / i), 3)
-# print(my_list)
-
-# n = int(input('Введите число N -> '))        +++
-# my_list = [round((1+1/i)**i, 3) for i in range(1, n+1)]
-# sum = round(sum(my_list), 3)
-# print(f'{my_list} {sum}')
-
 n = int(input('Введите число N -> '))
 my_list = []
 for i in range(1, n+1):
